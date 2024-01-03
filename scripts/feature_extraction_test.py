ffb5872d074e6bf91f0732b7dae1fa5f19466000
arupjana7365@gmail.com <arupjana7365@gmail.com>
2024-01-03 17:44:01 +0530

    hog tests

--- scripts/feature_extraction_test.py
+++ scripts/feature_extraction_test.py
@@ -1,22 +1,30 @@
 from pathlib import Path
 import time
 from feature_extraction.hog_feature_extractor import HistogramOrientedGradients
+from skimage.feature import hog
+from PIL import Image
 
 if __name__=="__main__":
 
-    image_paths = ['./data/test_images/bus-1.jpg', './data/test_images/bus-2.jpg', './data/test_images/car-1.jpg', './data/test_images/car-2.jpg', './data/test_images/truck-1.jpg', './data/test_images/truck-2.jpg']
+    image_path = './data/test_images/bus-2.jpg'
 
     HOG = HistogramOrientedGradients()
 
-    print("Starting feature extraction...")
+    img_path = Path(image_path)
+    features = HOG.hog(img_path)
+    print(f"Feature shape: {features.shape}")
 
-    for img_path in image_paths:
-        start_time = time.time()
 
-        img_path = Path(img_path)
-        features = HOG.hog(img_path)
-        print(f"Feature shape: {features.shape}")
+    start_time = time.time()
+    features = HOG.hog(img_path)
+    end_time = time.time()
 
-        end_time = time.time()
+    print(f"Time taken by custom hog method: {end_time-start_time}")
 
-        print(f"Time taken to perform feature extraction operation: {end_time-start_time}")
+    start_time = time.time()
+    img = Image.open(image_path)
+    img = img.resize((128, 64))
+    features = hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=False, channel_axis=-1)
+    end_time = time.time()
+    print(f"Time taken by skimage hog method: {end_time - start_time}")
+    print(f"Feature shape: {features.shape}")
